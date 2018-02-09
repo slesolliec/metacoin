145c4e070eb092cea4eee56456604ca9c24ec813
Stephane Le Solliec <slesolliec@gmail.com>
2018-02-09 16:47:20 +0100

    Importing token contracts from Ethereum.org

---
+++ contracts/owned.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.0;
+
+
+// from https://ethereum.org/token
+
+contract owned {
+    address public owner;
+
+    function owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+}
