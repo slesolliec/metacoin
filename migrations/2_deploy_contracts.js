8b26fb2c1af2061d9d3da9280c785b1ae86e97af
slesolliec <slesolliec@gmail.com>
2018-02-19 23:42:26 +0100

    Change tested ERC20 contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
 // var ConvertLib = artifacts.require("./ConvertLib.sol");
 // var MetaCoin = artifacts.require("./MetaCoin.sol");
 
-var MyAdvancedToken = artifacts.require("./MyAdvancedToken.sol");
+//  var MyAdvancedToken = artifacts.require("./MyAdvancedToken.sol");
+var MyERC20Token = artifacts.require("./TokenERC20.sol");
 
 module.exports = function(deployer) {
 //  deployer.deploy(ConvertLib);
 //  deployer.link(ConvertLib, MetaCoin);
 //  deployer.deploy(MetaCoin);
 
-    deployer.deploy(MyAdvancedToken,100,'EuroCoins','EUC');
+//    deployer.deploy(MyAdvancedToken,100,'EuroCoins','EUC');
+    deployer.deploy(MyERC20Token,100,'EuroCoins','EUC');
 
 };
